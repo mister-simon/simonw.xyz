056a70d614a55ea935e104a16d08f659a6e03944
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-08-11 18:01:50 +0100

    Refs #2, #3, polishing up the basic index pages for the blog section a little.

--- src/routes/thoughts/+page.svelte
+++ src/routes/thoughts/+page.svelte
@@ -5,6 +5,19 @@
 <div class="relative w-full overflow-clip">
 	<div class="mx-8 prose py-8">
 		<h2>Thoughts</h2>
-		<p>I wrote a bunch of things. I'll need to do some work to move them over here probably.</p>
+		<p>
+			Sometimes the things I think about fall out of my head, via my keyboard, into this little
+			repository of intrigues.
+		</p>
+		<p>
+			The only way to begin is by beginning, so without further ado, lets begin by reading <a
+				href="/thoughts/1.a-fresh-start.svx">about this site's new beginning</a
+			>.
+		</p>
+		<p>
+			I wrote 35 posts for the old version of this site, made with Nuxt, which can be found in the <a
+				href="/thoughts/archive">archive</a
+			>.
+		</p>
 	</div>
 </div>
--- src/routes/thoughts/archive/+page.svelte
+++ src/routes/thoughts/archive/+page.svelte
@@ -10,9 +10,30 @@
 			sveltekit and as a result, all the old Vue components no longer function. I intend to fix that
 			at some point, but until then, expect some things in here to be a little iffy.
 		</p>
-		<h2>My Favourite Posts</h2>
+		<h2>Some Favourite Posts</h2>
 		<ul>
-			<li></li>
+			<li>
+				<a href="/thoughts/archive/26.maybe-svelting-soon.svx">26.maybe-svelting-soon.svx</a> - I thought
+				"maybe I'll even start using it"... and here we are.
+			</li>
+			<li>
+				<a href="/thoughts/archive/28.svelting.svx">28.svelting.svx</a> - The follow up: "Who knows,
+				perhaps one day this whole site will come back again as a Svelte 5 app."
+			</li>
+			<li>
+				<a href="/thoughts/archive/31.we-nixing.svx">31.we-nixing.svx</a> - Early thoughts around switching
+				to NixOS. Still loving it.
+			</li>
+			<li>
+				<a href="/thoughts/archive/32.septembit.svx">32.septembit.svx</a> - I took a creative leap when
+				I committed to doing Septembit. As I write this it's mid-august, so there's another one coming
+				up. We'll see what happens.
+			</li>
+			<li>
+				<a href="/thoughts/archive/34.coding-a-weird-clock.svx">34.coding-a-weird-clock.svx</a> - When
+				the web is your job, finding real joy in side-projects isn't always easy. But this one really
+				brought me joy and I struggled to put it down.
+			</li>
 		</ul>
 	</div>
 </div>
