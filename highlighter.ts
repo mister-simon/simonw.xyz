8fd0a1b2bf1ac76e7aad7bd96a88ed01bc53eeee
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-09-04 17:44:28 +0100

    Extracting mdsvex config

---
+++ highlighter.ts
@@ -0,0 +1,24 @@
+import { transformerNotationDiff } from '@shikijs/transformers';
+import { transformerNotationHighlight } from '@shikijs/transformers';
+import { bundledLanguages, getSingletonHighlighter } from 'shiki';
+import { escapeSvelte } from 'mdsvex';
+
+import theme from 'shiki/themes/material-theme-ocean.mjs';
+
+async function highlighter(code: string, lang: string | null | undefined) {
+	const shiki = await getSingletonHighlighter({
+		themes: [theme],
+		langs: Object.keys(bundledLanguages)
+	});
+
+	const html = escapeSvelte(
+		shiki.codeToHtml(code, {
+			lang: lang ?? 'text',
+			theme,
+			transformers: [transformerNotationDiff(), transformerNotationHighlight()]
+		})
+	);
+	return `{@html \`${html} \`}`;
+}
+
+export default highlighter;
