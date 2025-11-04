fe76eb84a78678fe4fb7b70d78512f3138784b2b
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-11-04 15:43:57 +0000

    Adds actual contact options

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,6 +5,68 @@
 <div class="relative w-full overflow-clip">
 	<div class="mx-8 prose py-8">
 		<h1>Contact</h1>
-		<p>You can email me.</p>
+
+		<p>
+			I'd love to hear from you for any reason: If you've enjoyed looking around the site, if you
+			want to work with me, if you have a fun story and noone else will listen to it, whatever.
+		</p>
+
+		<ul class="pl-0">
+			<li class="flex items-center gap-4">
+				<span class="icon iconify size-5 pixel--envelope"></span>
+				<span>
+					No spam pls -
+					<a href="mailto:simonwxyz@duck.com" class="whitespace-nowrap">simonwxyz@duck.com</a>
+				</span>
+			</li>
+			<li class="flex items-center gap-4">
+				<span class="icon iconify size-5 pixel--bluesky"></span>
+				<span>
+					Say "Hi" on Bsky -
+					<a
+						href="https://bsky.app/profile/simonw.xyz"
+						target="_blank"
+						rel="noopener"
+						class="whitespace-nowrap">@simonw.xyz</a
+					>
+				</span>
+			</li>
+			<li class="flex items-center gap-4">
+				<span class="icon iconify size-5 pixel--github"></span>
+				<span>
+					Be a nosy git on my Github -
+					<a
+						href="https://github.com/mister-simon"
+						target="_blank"
+						rel="noopener"
+						class="whitespace-nowrap">mister-simon</a
+					>
+				</span>
+			</li>
+			<li class="flex items-center gap-4">
+				<span class="icon iconify size-5 pixel--twitch"></span>
+				<span>
+					Join an incredibly infrequent Twitch stream -
+					<a
+						href="https://www.twitch.tv/mister_simon"
+						target="_blank"
+						rel="noopener"
+						class="whitespace-nowrap">mister-simon</a
+					>
+				</span>
+			</li>
+			<li class="flex items-center gap-4">
+				<span class="icon iconify size-5 pixel--youtube"></span>
+				<span>
+					I technically exist on Youtube -
+					<a
+						href="https://www.youtube.com/@simonwxyz"
+						target="_blank"
+						rel="noopener"
+						class="whitespace-nowrap">@simonwxyz</a
+					>
+				</span>
+			</li>
+		</ul>
 	</div>
 </div>
